67c3587269704f62bc179842123d3bf30a772952
joshin-run <jahmsc@gmail.com>
2017-06-13 22:46:33 -0600

    Feat (js) now working

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //Note: jQuery is allowed but not required.
 
-window.onload = app.common.init();
-
 var app = app || {};
 app.common = {
   setImageAttributes: function() {
     alert('set attributes now working');
   },
   bindUIelements() {
-    alert('now working');
+    // alert('now working');
   },
   init: function() {
     this.bindUIelements();
   }
 }
+
+window.onload = app.common.init();
